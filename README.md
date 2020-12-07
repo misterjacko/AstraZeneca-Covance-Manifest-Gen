8c6dc4bf697d5c1ac01364c9ff407acdbf82d056
Jakob Ondrey <ondrey@gmail.com>
2020-12-06 21:33:10 -0600

    MVP commit. Working prototype.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # AstraZeneca-Covance-Manifest-Gen
-This is to facilitate the generation of Shipments to Covance Central Laboratories for the AstraZeneca COVID-19 vaccine trial.
+
+**\*Use at your own risk. This tool is to falitate the generation of a simples
+hipping manifest but it remains your responsibility to care for your samples
+and their cold-chain (if applicable). You are ultimatly responsible for the
+paperwork and samples that pass through your lab.**
+
+___
+
+## Why?
+
+During clinical trials, samples are often collected at locations throughout
+the worls and sent to central refrence laboratories for testing. In the case
+of the AstraZeneca COVID-19 vaccine trial, a variety of different samples are
+collected from each trial participant during multiple visits. These multiple
+samples often arrive in the lab where they are processed and separated to
+accomidate the temporary storage temperatures, spec. tube sizes, and shipping
+schedules. When the samples are shipped to Covance Central Laboratory, they
+need to be re-paired with their requisitions (or copies of the requisitions)
+so that the central lab knows what they are and what to do with them. 
+
+Due to patient confidentiality requirements and the good clinical trial
+practice of blinding patient samples, the samples are only labeled with a
+barcode connecting it to the requisition. This greatly complicates the pairing
+up of requisition and sample as people are generally bad at reading barcodes
+or otherwise matching up 10 -12 digit numbers. 
+
+Thankfully, we have computers that are pretty good at that. 
+
+---
+## Initial Functionality
+
+In its current form, this project consists of just an html page for data input
+and display and a javascript file for data manipulation. It can be run locally
+by placing the files in the same folder and opening index.html (you dont even
+need to be connected to the internet), or by using a web version [HERE](http://astrazeneca-covid19-shipping-manifest.s3-website-us-east-1.amazonaws.com/)
+
+Barcodes are scanned, filtered to just be numbers (some barcode readers will
+add letters to the input), and added to dictionaries where they can be paired
+with their respective aliquots or requisitions (depending on which order they
+are scanned in)
+
+Barcodes can be scanned to remove them from the manifest. This is especially
+useful if you have scanned in a requisition that should not be a part of the
+shipment. 
+
+Nothing is submitted or stored. This means that if the page is refreshed the
+data will be lost. 
---
+++ functions.js
@@ -0,0 +1,128 @@
+var dict = {};
+var unmatched = {};
+var requisitionLength = 3;
+var aliquotLength = 5
+
+function ScannerDelete(){
+  var deletecode = document.getElementById("deletecode").value;
+  deletecode = cleanString(deletecode);
+  if (deletecode.length == requisitionLength) {
+    if (deletecode in dict) {
+      if (dict[deletecode] == "") {
+        alert ('Deleting UNMATCHED requisition');
+      } else {
+        alert ('Deleting requisition AND matched aliquot');
+      }
+      delete dict[deletecode];
+    } else if (deletecode in unmatched) {
+      alert ("that requisition was not scanned so it cant be deleted. BUT it has a matching 'unmatched' aliquot!")
+    } else {
+      alert ("That requisition was not part of the manifest so it can't be deleted.")
+    }
+  } else if (deletecode.length == aliquotLength) {
+    var tempReq = deletecode.slice(0, -2);
+    if (tempReq in unmatched) {
+      alert ('Deleting UNMATCHED aliquot');
+      delete unmatched[tempReq];
+    } else if (tempReq in dict) {
+      if (dict[tempReq] == '') {
+        alert ("That aliquot was not scanned in so it can't be deleted. BUT it has a matching requisition!");
+      } else if (dict[tempReq] == deletecode) {
+        alert ("Deleting aliquot. You will also need to delete the requisition");
+        dict[tempReq] = '';
+      } else {
+        alert ('exception');
+      }
+    }
+  }
+  updateScreen()
+}
+
+function ScannerInput() {
+  var barcode = document.getElementById("barcode").value;
+  barcode = cleanString(barcode);
+  if (barcode.length == requisitionLength) {
+    if (!(barcode in dict)) {
+      dict[barcode] = checkunmatched(barcode);
+    } else {
+
+      alert('That requistition has already been scanned!');
+    }
+  } else if (barcode.length == aliquotLength) {
+    var tempReq = barcode.slice(0, -2);
+    if (dict[tempReq] == ""){
+      dict[tempReq] = barcode;
+    } else if (dict[tempReq] == barcode){
+      alert ("Already scanned for this requisition");
+    } else if (!(tempReq in dict)) {
+      unmatched[barcode.slice(0, -2)]= barcode;
+    }
+  } else {
+    var alertstring = "Scanned code outside the expected range of ";
+    alertstring += requisitionLength + " characters for a requisition ";
+    alertstring += "and " + aliquotLength + " characters for an aliquot.";
+    alert (alertstring);
+  }
+  updateScreen()
+}
+
+function updateScreen() {
+  document.getElementById("barcode").value = ""
+  document.getElementById("deletecode").value = ""
+  document.getElementById('UnmatchedOutput').innerHTML = '';
+  document.getElementById('ScannedOutput').innerHTML = '';
+  printunmatched(unmatched, 'UnmatchedOutput');
+  printmatched(dict, 'ScannedOutput');
+}
+function checkunmatched(barcode) {
+  if (barcode in unmatched) {
+    var returnval = unmatched[barcode];
+    delete unmatched[barcode];
+    document.getElementById('UnmatchedOutput').innerHTML = '';
+    printunmatched(unmatched, 'UnmatchedOutput');
+    return returnval;
+  } else {
+    return '';
+  }
+}
+
+function printmatched(dictionary, htmlelement) {
+  var dictLength = Object.keys(dictionary).length;
+  var i;
+  var stringblock;
+  for (i = 0; i < dictLength; i++) {
+    var thiskey; 
+    var thisvalue;
+    var thisString;
+    thiskey = Object.keys(dictionary)[i];
+    thisvalue = dictionary[thiskey];
+    thisString = thiskey + ' ---- ' + thisvalue;
+    stringblock = [stringblock
+      , thisString
+    ].join("\n");
+    document.getElementById(htmlelement).innerHTML = stringblock;
+  }
+}
+
+function printunmatched(dictionary, htmlelement) {
+  var dictLength = Object.keys(dictionary).length;
+  var i;
+  var stringblock;
+  for (i = 0; i < dictLength; i++) {
+    var thiskey; 
+    var thisvalue;
+    var thisString;
+    thiskey = Object.keys(dictionary)[i];
+    thisvalue = dictionary[thiskey];
+    thisString = thisvalue;
+    stringblock = [stringblock
+      , thisString
+    ].join("\n");
+    document.getElementById(htmlelement).innerHTML = stringblock;
+  }
+}
+
+function cleanString(string) {
+  string = string.replace(/\D+/g, '');
+  return string
+}
